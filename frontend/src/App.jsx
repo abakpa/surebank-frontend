37d69c28da8d794a24087afdd7d629f546915589
joseph abakpa <elachi4c@yahoo.com>
2025-04-29 07:23:59 +0100

    index8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes,Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Login from "./Components/CustomerLogin";
 import CustomerAccountDashboard from "./Components/CustomerAccountDashboard";
@@ -49,8 +49,7 @@ function App() {
             toggleSidebar={toggleSidebar}
           />
         <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
           <Route path="/customeraccountdashboard/:customerId" element={<CustomerAccountDashboard />} />
         </Routes>
         </div>
